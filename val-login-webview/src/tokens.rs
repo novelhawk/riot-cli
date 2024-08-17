d7f8145a66f1dd3c6f91cfa8bfb478d093daf22c
novelhawk <novelhawk@gmail.com>
2024-08-17 16:53:38 +0200

    chore: remove legacy projects

--- val-login-webview/src/tokens.rs
+++
@@ -1,8 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Tokens {
-    pub access_token: String,
-    pub id_token: String,
-    pub expires_in: u64,
-}
